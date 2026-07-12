357face8737eedc65fbc51767c9177a0b1f56abf
sfrabby <fazlerabby6732@gmail.com>
2026-07-12 23:26:56 +0600

    List

---
+++ List_Map.dart
@@ -0,0 +1,6 @@
+void main(){
+  List myList = ['Molhali', 'abdullahpur', 'tongi', 'Gajipur', 'Sirajgong'];
+  print(myList.length);
+  print("all bus stop list ${myList}");
+  print("1st bus stop list ${myList[0]}");
+}
---
+++ String.dart
@@ -0,0 +1,15 @@
+
+String Name = "Fazle";
+String Passion = "App Developer";
+String Email = "fazlerabby6732@gmail.com";
+void main(){
+//String intro = "My name is ${Name} I  am ${Passion}, please contact me  ${Email}";
+
+String Multilne_intro = '''My name is ${Name} 
+I  am ${Passion}, 
+please contact me  ${Email} ''';
+print(Multilne_intro);
+
+
+
+}
