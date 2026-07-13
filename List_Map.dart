d6a30771c2865e60faadc56a93eca5f680dd852a
sfrabby <fazlerabby6732@gmail.com>
2026-07-13 19:15:44 +0600

    Map in dart

--- List_Map.dart
+++ List_Map.dart
@@ -1,6 +1,33 @@
 void main(){
   List myList = ['Molhali', 'abdullahpur', 'tongi', 'Gajipur', 'Sirajgong'];
-  print(myList.length);
-  print("all bus stop list ${myList}");
-  print("1st bus stop list ${myList[0]}");
+  //print(myList.length);
+  //print("all bus stop list ${myList}");
+  //print("1st bus stop list ${myList[0]}");
+
+
+  Map<String ,String> myDetails = {
+    "Name" : "Fazle Rabby",
+    "Home Town" : "Rangpur",
+    "Email" : "fazlerabby6732@gmail.com",
+    "Contact" : "01765944937",
+  };
+
+  print(myDetails);
+
+  List<Map<String,  String>> myDetails2 = [
+    {
+    "Name" : "Rafi",
+    "Home Town" : "Rangpur",
+    "Email" : "fazlerabby6732@gmail.com",
+    "Contact" : "01765944937",
+  },
+  {
+    "Name" : "Fazle Rabby",
+    "Home Town" : "Rangpur",
+    "Email" : "fazlerabby6732@gmail.com",
+    "Contact" : "01765944937",
+  }
+  ];
+print(myDetails2[0]["Name"]);
+
 }
