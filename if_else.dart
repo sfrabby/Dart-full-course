e9dc631d2782fa81f554cb63f836c4fa6329a90f
sfrabby <fazlerabby6732@gmail.com>
2026-07-14 22:52:23 +0600

    Dart condition

---
+++ if_else.dart
@@ -0,0 +1,29 @@
+void main(){
+
+  var dayOfweek = 3;
+  if(dayOfweek ==1){
+    print("This is Saturday");
+  }
+  else if(dayOfweek==2){
+    print("This is Sunday");
+  }else if (dayOfweek ==3){
+    print("This is Saturday");
+  }
+  else if(dayOfweek  ==  4){
+    print("This is Monday");
+  
+  }
+   else if(dayOfweek  ==  5){
+    print("This is Tuewsday");
+  
+  }
+   else if(dayOfweek  ==  6){
+    print("This is Thursday");
+  
+  }
+   else if(dayOfweek  ==  7){
+    print("This is Firday");
+  
+  }
+  
+}
